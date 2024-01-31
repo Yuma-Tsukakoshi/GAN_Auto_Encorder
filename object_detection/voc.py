514d482c657956e8a82f511f4090c762b556b59d
Yuma-Tsukakoshi <yttn9984kawa@gmail.com>
2024-01-31 14:55:02 +0900

    add:データの前処理

--- object_detection/voc.py
+++ object_detection/voc.py
@@ -55,3 +55,84 @@ def make_filepath_list(rootpath):
     return train_img_list, train_anno_list, val_img_list, val_anno_list
 
 
+"""
+2. バウンディングボックスの座標と正解ラベルをリスト化するclass
+"""
+import xml.etree.ElementTree as ElementTree # xmlファイルを読み込むためのライブラリ
+import numpy as np
+
+class GetBBoxAndLabel(object):
+    """
+    1枚の画像のアノテーション(座標とラベル)をnumpy配列で返す
+    
+    Attributes
+    ----------
+    classes : list
+        VOCのクラス名を格納したリスト
+    """
+        
+    def __init__(self, classes):
+        """
+        Parameters
+        ----------
+        classes : list
+            VOCのクラス名を格納したリスト
+        """
+        self.classes = classes
+        
+    def __call__(self, xml_path, width, height):
+        """
+        インスタンスから呼び出すときに実行される関数
+        
+        Parameters
+        ----------
+        xml_path : str
+            xmlファイルのパス
+        width : int
+            画像の幅 正規化に必要
+        height : int
+            画像の高さ 正規化に必要
+            
+        Returns(ndarray)
+        -------
+        [[xmin, ymin, xmax, ymax, label_ind], ... ]
+        要素数は画像内に存在する物体数と同じ
+        """
+        
+        annotation = []
+    
+        xml = ElementTree.parse(xml_path).getroot() # xmlファイルを読み込む .getroot()でxmlのルート要素を取得
+        
+        for obj in xml.iter('object'):
+            # アノテーションで検知がdifficultに設定されているものは除外
+            difficult = int(obj.find('difficult').text)
+            if difficult == 1:
+                continue
+            
+            # 1つの物体に対するアノテーションを格納するリスト
+            bndbox = []
+            
+            # バウンディングボックスのラベルをリストに格納
+            name = obj.find('name').text.lower().strip()
+            bbox = obj.find('bndbox')
+            
+            grid = ['xmin', 'ymin', 'xmax', 'ymax']
+            
+            for gr in (grid):
+                axis_value = int(bbox.find(gr).text) -1
+                
+                if gr == 'xmin' or gr == 'xmax':
+                    axis_value /= width
+                
+                else:
+                    axis_value /= height
+                    
+                bndbox.append(axis_value)
+            
+            # 物体名のインデックスを格納
+            label_idx = self.classes.index(name)
+            bndbox.append(label_idx)
+            
+            annotation += [bndbox]
+            
+        return np.array(annotation)
